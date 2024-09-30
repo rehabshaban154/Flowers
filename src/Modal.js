28993eb5f6ebef367e45c83e21ed3dc76cebb861
Re12345678 <rehabshaban245@gmail.com>
2024-10-01 02:53:40 +0300

    new commit

--- src/Modal.js
+++ src/Modal.js
@@ -8,7 +8,7 @@ export default function Modal({ isOpen, onClose, item }) {
     <div className="modal-overlay">
       <div className="modal-box">
         <button className="close-btn close" onClick={onClose}>X</button>
-        <img src={item.image} alt={item.title} className="modal-image" />
+        <img src={item.img} alt={item.title} className="modal-image" />
         <h2 style={{color:'var(--alt)'}}>{item.title}</h2>
         <h3 style={{color:'#FDD835'}}>{item.price}</h3>
         <p>{item.description}</p> {/* Assuming each item has a description */}
